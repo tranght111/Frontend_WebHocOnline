b6a37b7d9bb89f414bf8d18f055758f9cdccb7f5
TRANG HOÀNG THỊ THÙY <18120605@student.hcmus.edu.vn>
2022-01-22 10:18:22 +0700

    Update SignUp UI

--- src/pages/SignUp.vue
+++ src/pages/SignUp.vue
@@ -1,61 +1,6 @@
 <template>
     <div>
 
-        <!-- preloader end  -->
-    <header>
-        <div id="theme-menu-two" class="main-header-area main-head-three pl-100 pr-100 pt-20 pb-15">
-            <div class="container-fluid">
-                <div class="row align-items-center">
-                        <div class="col-xl-2">
-                            <div class="logo"><a href="index.html"><img src="assets/img/logo/header_logo_one.svg" alt=""></a></div>
-                        </div>
-                        <div class="col-xl-7">
-                            <nav class="main-menu navbar navbar-expand-lg justify-content-center">
-                                <div class="nav-container">
-                                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                                        <ul class="navbar-nav">
-                                            <li class="nav-item dropdown mega-menu">
-                                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                                Danh mục
-                                                </a>
-                                                <ul class="dropdown-menu submenu mega-menu__sub-menu-box" aria-labelledby="navbarDropdown">
-                                                <li><a href="index.html"><span><img src="assets/img/icon/icon7.svg" alt=""></span> Business</a></li>
-                                                <li><a href="index-2.html"><span><img src="assets/img/icon/icon8.svg" alt=""></span> Technology</a></li>
-                                                <li><a href="index.html"><span><img src="assets/img/icon/icon9.svg" alt=""></span> Development</a></li>
-                                                <li><a href="index-2.html"><span><img src="assets/img/icon/icon10.svg" alt=""></span> Photography</a></li>
-                                                <li><a href="index.html"><span><img src="assets/img/icon/icon11.svg" alt=""></span> Digital Marketing</a></li>
-                                                <li><a href="index-2.html"><span><img src="assets/img/icon/icon12.svg" alt=""></span> Programming</a></li>
-                                                <li><a href="index.html"><span><img src="assets/img/icon/icon14.svg" alt=""></span> Videograpgy</a></li>
-                                                <li><a href="index-2.html"><span><img src="assets/img/icon/icon13.svg" alt=""></span> Illustration</a></li>
-                                            </ul>
-                                            </li>
-                                            <li class="nav-item">
-                                                <a class="nav-link" href="/#/" id="navbarDropdown5" role="button"  aria-expanded="false">Trang chủ</a></li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </nav>
-                        </div>
-                        <div class="col-xl-3 col-lg-2 col-7">
-                            <div class="right-nav d-flex align-items-center justify-content-end">
-                                <div class="right-btn mr-25 mr-xs-15">
-                                    <ul class="d-flex align-items-center">
-                                        <li><a href="/#/Signup" class="theme_btn free_btn">Đăng ký ngay</a></li>
-                                        <li><a class="sign-in ml-20" href="/#/Login"><img src="assets/img/icon/user.svg" alt=""></a></li>
-                                    </ul>
-                                </div>
-                                <div class="hamburger-menu d-md-inline-block d-lg-none text-right">
-                                    <a href="javascript:void(0);">
-                                        <i class="far fa-bars"></i>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-            </div>
-        </div> <!-- /.theme-main-menu -->
-    </header>
-
         <section class="what-looking-for pos-rel gradient-bg pt-135 pb-115 pt-md-90 pb-md-70 pt-xs-90 pb-xs-70">
   <div class="what-blur-bg-three"></div>
   <div class="container">
